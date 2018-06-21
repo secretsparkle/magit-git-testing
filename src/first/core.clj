326ba3c104541291bd61233623fe960cadef106f
secretsparkle <scott.madera@datastax.com>
2018-06-21 10:48:14 -0700

    Added my-map function, which does the same thing as the standard map function (but probably not as efficiently).

--- src/first/core.clj
+++ src/first/core.clj
@@ -12,3 +12,10 @@
     (if (empty? coll)
       acc
       (recur (rest coll) (+ (first coll) acc)))))
+
+(defn my-map [func lst]
+  (loop [coll lst
+         acc nil]
+    (if (empty? coll)
+      (reverse acc)
+      (recur (rest coll) (cons (func (first coll)) acc)))))
