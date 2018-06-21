1d5b82166646a215e896be10b01461e2dcc0434f
secretsparkle <scott.madera@datastax.com>
2018-06-20 17:26:39 -0700

    Added an adding function that uses loop that runs through any number of supplied arguments and sums them up.

--- src/first/core.clj
+++ src/first/core.clj
@@ -5,3 +5,10 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn add [& args]
+  (loop [coll args
+         acc 0]
+    (if (empty? coll)
+      acc
+      (recur (rest coll) (+ (first coll) acc)))))
