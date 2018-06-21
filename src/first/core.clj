5810151dbfa52afc45e0909a5e3cb578b1341c62
secretsparkle <scott.madera@datastax.com>
2018-06-21 15:57:30 -0700

    Added my-reduce and elem functions

--- src/first/core.clj
+++ src/first/core.clj
@@ -19,3 +19,22 @@
     (if (empty? coll)
       (reverse acc)
       (recur (rest coll) (cons (func (first coll)) acc)))))
+<<<<<<< HEAD
+=======
+
+(defn my-reduce [func init lst]
+  (loop [coll lst
+         acc init]
+    (if (empty? coll)
+      acc
+      (recur (rest coll) (func (first coll) acc)))))
+
+(defn elem [elt lst]
+  (loop [piece elt
+         whole lst]
+    (if (empty? whole)
+      nil
+      (if (= piece (first whole))
+        true
+        (recur piece (rest whole))))))
+>>>>>>> 78360c1... Added elem function to check if element is part of a list.
